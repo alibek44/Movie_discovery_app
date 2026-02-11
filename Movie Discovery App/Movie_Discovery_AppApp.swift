64fc092a41f336b53e85aeccf88475de58e4b0fd
alibek <alibekbekkaliev6@gmail.com>
2026-02-12 02:16:09 +0500

    Initial Commit

---
+++ Movie Discovery App/Movie_Discovery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Discovery_AppApp.swift
+//  Movie Discovery App
+//
+//  Created by Alibek on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Discovery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
